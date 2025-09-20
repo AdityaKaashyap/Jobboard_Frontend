aa4e482235ffd84f429cbed3ce6575b23efba460
AdityaKaashyap <adityapk333@gmail.com>
2025-09-20 11:43:23 +0530

    Services updated

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { AuthProvider } from './contexts/AuthContext';
+import { NotificationProvider } from './contexts/NotificationContext';
+import { MessageProvider } from './contexts/MessageContext';
+
+// Placeholder reducer for now
+const store = configureStore({ reducer: {} });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <AuthProvider>
+        <NotificationProvider>
+          <MessageProvider>
+            <App />
+          </MessageProvider>
+        </NotificationProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
